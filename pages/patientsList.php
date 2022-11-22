1a48c63ea9aa4f2befb1b95985f44095b7a03675
shindhha <99216728+shindhha@users.noreply.github.com>
2022-11-22 19:45:34 +0100

    "Barre d'outils initialiser"

--- pages/patientsList.php
+++ pages/patientsList.php
@@ -36,32 +36,38 @@
 			<div class="col-md-11 h-75 text-center">
 				<!-- Bandeau outils -->
 
-				<div class="row h-15 align-items-center green">
-					<div class="d-flex">
+				<div class="row h-15 green">
+					<div class="d-flex justify-content-between align-items-center px-5">
 						<img onclick="manageClass('menu','d-none')" class="d-md-none d-sm-block sizeIcon" src="res/menu.svg">
-						<span class="fs-1 d-md-block d-none"> Page d'Accueil </span>
+						<span class="fs-1 d-md-block d-none"> Liste Patients </span>
+						<div class="d-flex flex-row align-items-center gap-4">
+							<div class="d-flex border-1 bg-white px-2 py-2">
+								<input name="keyWords" type="text" placeholder="Mots clefs" class="border-0 ">
+								<span class="material-symbols-outlined text-dark pointer">search</span>
+
+							</div>
+							 <div class="d-flex">
+								 <span> Filtres </span>
+								 <span  onclick="manageClass('param','d-none')" class="material-symbols-outlined pointer"> expand_more </span>
+								 <div id="param" class="bg-white d-none position-absolute bottom-75"> dqsdqsdqs</div>
+							 </div>
+							 <span class="material-symbols-outlined pointer"> autorenew </span>
+						</div>
+
 					</div>
-					
+
 
 				</div>
-				<span class="fs-1 d-md-none d-sm-block text-green"> Page d'Accueil </span>
+				<span class="fs-1 d-md-none d-sm-block text-green"> Liste Patients </span>
 				<!-- content -->
 				<div class="row h-100 align-items-center text-center">
-					<!-- Portail de connexion -->
+
 					<div class="container ">
 						<div class="row justify-content-center">
 							<div class="col-md-8 col-xl-6 col-sm-7 col-12 green border-2 p-5">
-								<form class="d-flex flex-column gap-3">
-									<span class="fs-1"> Connexion a <u>MEDILOG</u> </span>
+								<table>
 
-
-									<div class="d-flex gap-3"><img src="res/iaccount.svg" class="sizeIcon"><input type="text" placeholder="Identifiant" class="border-0 border-1 w-75 ps-2 pt-2 pb-2"></div>
-
-									<div class="d-flex gap-3"><img src="res/ipassword.svg" class="sizeIcon"><input type="password" placeholder="Mot de passe" class="ps-2 border-0 border-1 w-75 pt-2 pb-2"></input></div>
-
-									<div><button class="border-0 w-50 border-1 text-green fs-3"><u> Valider </u></button></div>
-									<a class="fs-6 text-white"> Mot de passe oublié </a>
-								</form>
+								</table>
 							</div>
 
 						</div>
