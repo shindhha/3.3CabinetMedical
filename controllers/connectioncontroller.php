29c6c805f8fe5aa8d8fa199b689e5504ac72962d
shindhha <99216728+shindhha@users.noreply.github.com>
2022-11-23 01:51:23 +0100

    Connexion avec YASMF correcte

---
+++ controllers/connectioncontroller.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * yasmf - Yet Another Simple MVC Framework (For PHP)
+ *     Copyright (C) 2019   Franck SILVESTRE
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace controllers;
+
+use yasmf\View;
+use services\LoginService;
+use yasmf\HttpHelper;
+/**
+ * yasmf - Yet Another Simple MVC Framework (For PHP)
+ *     Copyright (C) 2019   Franck SILVESTRE
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as published
+ *     by the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+class ConnectionController
+{
+  private $LoginService;
+
+  public function __construct()
+  {
+      $this->LoginService = LoginService::getDefaultUsersService();
+  }
+    public function index($pdo) {
+      $login = HttpHelper::getParam('login');
+      $password = htmlspecialchars(HttpHelper::getParam('password'));
+      $searchStmt = $this->LoginService->findIfAdminExists($pdo, $login, $password) ;
+      $view = new View("Sae3.3CabinetMedical/views/connection");
+
+      if ($searchStmt) {
+        $view = new View("Sae3.3CabinetMedical/views/patientslist");
+      }
+
+        return $view;
+    }
+
+}
--- views/connection.php
+++ views/connection.php
@@ -3,20 +3,46 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="../css/styles.css">
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0" />
-	<script type="text/javascript" src="../scripts/script.js"></script>
+	<script type="text/javascript" src="scripts/script.js"></script>
 	<title>MEDILOG</title>
 	<?php
+	spl_autoload_extensions(".php");
+	spl_autoload_register();
+	use yasmf\HttpHelper;
+
+
 		$hostname = "sql.alphaline.ml";
 		$dbname = "SAE";
 		$user = "guillaume";
 		$password = "guillaume";
+		$port = 3306;
+		$charset = "utf8";
+
+		$pdo = new PDO('mysql:host='.$hostname.';dbname='.$dbname.';charset=utf8', $user,$password);
+
+		$request = $pdo->prepare("SELECT * FROM user WHERE id = :id AND mdp = :password");
+		$connectionWellDone = false;
+
+		if (isset($_POST['id']) && isset($_POST['password'])) {
+			global $request;
+
+			$request->execute(array('id' => $_POST['id'], 'password' => $_POST['password']));
+			$num = $request->rowcount();
+			$connectionWellDone = ($num == 1);
+
+		}
+
+		if($connectionWellDone) {
+			header("Location: pages/patientsList.php");
+		}
+
 	?>
 </head>
-
 <body onload="resizeMenu()">
+	<input name="controller" type="text" value="connection">
 	<div class="container-fluid h-100  text-white">
 		<div class="row h-100">
 			<!-- Menu -->
@@ -41,7 +67,7 @@
 						<img onclick="manageClass('menu','d-none')" class="d-md-none d-sm-block sizeIcon" src="res/menu.svg">
 						<span class="fs-1 d-md-block d-none"> Page d'Accueil </span>
 					</div>
-					
+
 
 				</div>
 				<span class="fs-1 d-md-none d-sm-block text-green"> Page d'Accueil </span>
@@ -51,13 +77,13 @@
 					<div class="container ">
 						<div class="row justify-content-center">
 							<div class="col-md-8 col-xl-6 col-sm-7 col-12 green border-2 p-5">
-								<form class="d-flex flex-column gap-3">
+								<form method="POST" class="d-flex flex-column gap-3">
 									<span class="fs-1"> Connexion a <u>MEDILOG</u> </span>
 
 
-									<div class="d-flex gap-3"><img src="res/iaccount.svg" class="sizeIcon"><input type="text" placeholder="Identifiant" class="border-0 border-1 w-75 ps-2 pt-2 pb-2"></div>
+									<div class="d-flex gap-3"><img src="res/iaccount.svg" class="sizeIcon"><input name="login" type="text" placeholder="Identifiant" class="border-0 border-1 w-75 ps-2 pt-2 pb-2"></div>
 
-									<div class="d-flex gap-3"><img src="res/ipassword.svg" class="sizeIcon"><input type="password" placeholder="Mot de passe" class="ps-2 border-0 border-1 w-75 pt-2 pb-2"></input></div>
+									<div class="d-flex gap-3"><img src="res/ipassword.svg" class="sizeIcon"><input name="password" type="password" placeholder="Mot de passe" class="ps-2 border-0 border-1 w-75 pt-2 pb-2"></input></div>
 
 									<div><button class="border-0 w-50 border-1 text-green fs-3"><u> Valider </u></button></div>
 									<a class="fs-6 text-white"> Mot de passe oublié </a>
