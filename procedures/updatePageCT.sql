2ca09908c1ad6ae8dd3172e341aeb4db39c8ec7e
AstroNey <l.nicolas02@laposte.net>
2022-12-12 10:52:13 +0100

    update de la table HAS_LiensPageCT

---
+++ procedures/updatePageCT.sql
@@ -0,0 +1,20 @@
+DELIMITER //
+
+CREATE OR REPLACE FUNCTION updateCT(
+                    N_codeHAS VARCHAR(8),
+                    N_lienPage TEXT) RETURNS INT
+BEGIN
+    -- INFO : il faut inpérativement exécuter l'update des liens avant l'update
+    --        de SMR et ASMR pour que les liens ne soient pas vides.
+    DECLARE RETURN_CODE INT DEFAULT 0;
+
+    /* Si le lien n'existe pas, on le crée sinon on l'update  */
+    IF (SELECT COUNT(*) FROM HAS_LiensPageCT WHERE codeHAS = N_codeHAS) = 0 THEN
+        SELECT importCT(N_codeHAS, N_lienPage) INTO RETURN_CODE;
+    ELSE
+        UPDATE HAS_LiensPageCT SET lienPage = N_lienPage WHERE codeHAS = N_codeHAS;
+    END IF;
+
+    RETURN RETURN_CODE;
+
+end //
