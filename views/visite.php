8804daabb4533dbd16fa214356ed74e44361bc66
Alphaline <clementlaurie27@gmail.com>
2023-01-17 15:14:32 +0100

    Fix : Typo

--- views/visite.php
+++ views/visite.php
@@ -21,7 +21,7 @@
 		<div class="row h-100">
 			<!-- Menu -->
 			<div id="menu" class="pt-3 menu z-index-dropdown col-md-1 col-4 d-md-flex d-none flex-column gap-3 blue h-100 align-items-center">
-				<span onclick="manageClass('menu','d-none')"class="material-symbols-outlined d-block d-md-none text-end w-100">arrow_back</span>
+				<span onclick="manageClass('menu','d-none')" class="material-symbols-outlined d-block d-md-none text-end w-100">arrow_back</span>
 				<div class=" green border-1 ratio ratio-1x1">
 
 				</div>
@@ -52,7 +52,7 @@
                 </a>
 			</div>
 			<!-- Main page -->
-			<div class="col-md-11 h-75 text-center  ">
+			<div class="col-md-11 h-75 text-center h-11">
 				<!-- Bandeau outils -->	
 				<nav class="  row h-11 navbar navbar-expand-lg navbar-light green">
 					<div class="d-flex px-md-5 container-fluid green">
@@ -64,116 +64,115 @@
 							</form>
 					</div>
 				</nav>
-				<!-- Bandeau Patient -->
-				<div class="blue row">
-					<div class="d-flex justify-content-between">
-						<span></span>
-						<div><?php echo $patient['nom'] . " " . $patient['prenom']?></div>
-						<div></div>
-					</div>
-				</div>
-				<div class="row d-flex flex-column gap-3 mx-auto col-8">
-					<div class="d-flex flex-row justify-content-between text-dark px-5">				
-						<span>Motif : <?php echo $visite['motifVisite'] ; ?></span>
-						<span>Date :  <?php echo $visite['dateVisite']?></span>				
-					</div>
-					<div class="d-flex flex-column text-start text-dark d-flex flex-column gap-3">
-						Description
-						<div class="border border-dark px-2 pb-4 ">
-							<?php echo $visite['Description'] ?>
-						</div>
-						Conclusion
-						<div class="border border-dark px-2 pb-4">
-							<?php echo $visite['Conclusion'] ?>
-						</div>
-					</div>
-				</div>
-				<!-- content -->
-				<div class="row align-items-center text-center">
-					<!-- Portail de connexion -->
-					<div class="container h-100 ">
-						<div class="row justify-content-center h-75">
-							<div class=" col-md-10 col-xl-12 col-sm-7 col-12 success border-2">
-								<div class="text-danger text-start"><?php if(isset($addMedicError)) echo $addMedicError; ?></div>
-								<div class=" text-dark text-start h2"> Liste des medicaments prescrit </div>
-								<div class="table-responsive">
-									<table class="table table-striped lightGreen border border-dark h-50 overflow-scroll">
-										<thead class="sticky-top bg-white text-dark  ">
-											<tr>
-												<th>Designation</th>
-												<th>Presentation</th>
-												<th>Instruction	</th>
-												<th></th>
-											</tr>
-										</thead>
-										<?php
-											foreach ($drugsVisite as $row) {
-											echo "<tr>"
-												 ."<td>" . $row['designation'] . "</td>"
-												 ."<td>" . $row['libellePresentation'] . "</td>"
-												 ."<td>" . $row['instruction'] . "</td>";
-										?>
-										<td>
-										<div class="dropdown">
-											<span class="material-symbols-outlined" type="button" id="dropdownMenuButton1" data-bs-auto-close="false" data-bs-toggle="dropdown" aria-expanded="false">
-												more_horiz
-											</span>
-											<div class="p-0 text-end dropdown-menu dropdown-menu-end green text-white no-border" aria-labelledby="dropdownMenuButton1">
-												<table class="text-white ">
-													<form action="index.php" method="POST"  class="d-flex flex-column green text-end">
-														<input type="hidden" name="controller" value="medicamentslist">
-														<input type="hidden" name="action" value="goFicheMedicament">
-														<input type="hidden" name="codeCIP7" value="<?php echo $row['codeCIP7'] ?>">
-														<tr><input type="submit" class="btn text-white text-decoration-underline text-end" value="Afficher"> </tr>
-													</form>
-													<form action="index.php" method="POST" class="d-flex flex-column green">
-														<input type="hidden" name="controller" value="patientslist">
-														<input type="hidden" name="action" value="deleteMedicament">
-														<input type="hidden" name="codeCIP7" value="<?php echo $row['codeCIP7'] ?>">
-														<tr><input class="btn text-white text-decoration-underline text-end" type="submit" value="Supprimer"></tr>
-													</form>
-													
-														<a class="btn text-white text-decoration-underline text-end " data-bs-toggle="modal" href="#exampleModal" onclick="add('<?php echo $row['libellePresentation']."','". $row['codeCIP7'] . "','" . $row['instruction'] ?>')"  >Editer</a>
-													
-												</table>			
-											</div>
-										</div>
-										</td>
-										<?php
-										echo "</tr>";
-											} 
-										?>
-									</table>
-								</div>
-								<div class="d-flex flex-row justify-content-end ">
-									<div class="d-flex me-2 py-2 px-3 border-1 green">
-										<form>
-											<input type="hidden" name="controller" value="patientslist">
-											<input type="hidden" name="action" value="generatePdf">
-											<input type="submit" class="green no-border text-white"  value="Télécharger l'ordonnance">
-										</form>
-									</div>
-									<div class="d-flex me-2 py-2 px-3 border-1 green">
-										<form>
-											<input type="hidden" name="idVisite" value="<?php echo $visite['idVisite'] ?>">
-											<input type="hidden" name="controller" value="medicamentslist">
-											<input type="submit" class="green no-border text-white"  value="Ajouter un medicament">
-										</form>
-									</div>
-									<div class="d-flex me-2 py-2 px-3 border-1 green">
-										<form>
-											<input type="hidden" name="nextAction" value="updateVisite">
-											<input type="hidden" name="action" value="goEditVisite">
-											<input type="hidden" name="controller" value="patientslist">
-											<input type="submit" class="green no-border text-white" value="Modifier la visite">
-										</form>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-				
+                    <!-- Bandeau Patient -->
+                    <div class="blue row">
+                        <div class="d-flex justify-content-between">
+                            <span></span>
+                            <div><?php echo $patient['nom'] . " " . $patient['prenom']?></div>
+                            <div></div>
+                        </div>
+                    </div>
+
+                <div class="row d-flex flex-column gap-3 mx-auto col-8">
+                    <div class="d-flex flex-row justify-content-between text-dark px-5">
+                        <span>Motif : <?php echo $visite['motifVisite'] ; ?></span>
+                        <span>Date :  <?php echo $visite['dateVisite']?></span>
+                    </div>
+                    <div class="d-flex flex-column text-start text-dark d-flex flex-column gap-3">
+                        Description
+                        <div class="border border-dark px-2 pb-4 ">
+                            <?php echo $visite['Description'] ?>
+                        </div>
+                        Conclusion
+                        <div class="border border-dark px-2 pb-4">
+                            <?php echo $visite['Conclusion'] ?>
+                        </div>
+                    </div>
+                </div>
+                <!-- content -->
+                <div class="row align-items-center text-center">
+                    <div class="container h-100">
+                        <div class="row justify-content-center h-75">
+                            <div class=" col-md-10 col-xl-12 col-sm-7 col-12 success border-2">
+                                <div class="text-danger text-start"><?php if(isset($addMedicError)) echo $addMedicError; ?></div>
+                                <div class=" text-dark text-start h2"> Liste des medicaments prescrit </div>
+                                <div class="table-responsive h-75 overflow-scroll">
+                                    <table class="table table-striped lightGreen border border-dark">
+                                        <thead class="sticky-top bg-white text-dark  ">
+                                            <tr>
+                                                <th>Designation</th>
+                                                <th>Presentation</th>
+                                                <th>Instruction	</th>
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <?php
+                                            foreach ($drugsVisite as $row) {
+                                            echo "<tr>"
+                                                 ."<td>" . $row['designation'] . "</td>"
+                                                 ."<td>" . $row['libellePresentation'] . "</td>"
+                                                 ."<td>" . $row['instruction'] . "</td>";
+                                        ?>
+                                        <td>
+                                        <div class="dropdown">
+                                            <span class="material-symbols-outlined" type="button" id="dropdownMenuButton1" data-bs-auto-close="false" data-bs-toggle="dropdown" aria-expanded="false">
+                                                more_horiz
+                                            </span>
+                                            <div class="p-0 text-end dropdown-menu dropdown-menu-end green text-white no-border" aria-labelledby="dropdownMenuButton1">
+                                                <table class="text-white ">
+                                                    <form action="index.php" method="POST"  class="d-flex flex-column green text-end">
+                                                        <input type="hidden" name="controller" value="medicamentslist">
+                                                        <input type="hidden" name="action" value="goFicheMedicament">
+                                                        <input type="hidden" name="codeCIP7" value="<?php echo $row['codeCIP7'] ?>">
+                                                        <tr><input type="submit" class="btn text-white text-decoration-underline text-end" value="Afficher"> </tr>
+                                                    </form>
+                                                    <form action="index.php" method="POST" class="d-flex flex-column green">
+                                                        <input type="hidden" name="controller" value="patientslist">
+                                                        <input type="hidden" name="action" value="deleteMedicament">
+                                                        <input type="hidden" name="codeCIP7" value="<?php echo $row['codeCIP7'] ?>">
+                                                        <tr><input class="btn text-white text-decoration-underline text-end" type="submit" value="Supprimer"></tr>
+                                                    </form>
+
+                                                        <a class="btn text-white text-decoration-underline text-end " data-bs-toggle="modal" href="#exampleModal" onclick="add('<?php echo $row['libellePresentation']."','". $row['codeCIP7'] . "','" . $row['instruction'] ?>')"  >Editer</a>
+
+                                                </table>
+                                            </div>
+                                        </div>
+                                        </td>
+                                        <?php
+                                        echo "</tr>";
+                                            }
+                                        ?>
+                                    </table>
+                                </div>
+                                <div class="d-flex flex-row justify-content-end ">
+                                    <div class="d-flex me-2 py-2 px-3 border-1 green">
+                                        <form>
+                                            <input type="hidden" name="controller" value="patientslist">
+                                            <input type="hidden" name="action" value="generatePdf">
+                                            <input type="submit" class="green no-border text-white"  value="Télécharger l'ordonnance">
+                                        </form>
+                                    </div>
+                                    <div class="d-flex me-2 py-2 px-3 border-1 green">
+                                        <form>
+                                            <input type="hidden" name="idVisite" value="<?php echo $visite['idVisite'] ?>">
+                                            <input type="hidden" name="controller" value="medicamentslist">
+                                            <input type="submit" class="green no-border text-white"  value="Ajouter un medicament">
+                                        </form>
+                                    </div>
+                                    <div class="d-flex me-2 py-2 px-3 border-1 green">
+                                        <form>
+                                            <input type="hidden" name="nextAction" value="updateVisite">
+                                            <input type="hidden" name="action" value="goEditVisite">
+                                            <input type="hidden" name="controller" value="patientslist">
+                                            <input type="submit" class="green no-border text-white" value="Modifier la visite">
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 			</div>
 		</div>
 		<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
