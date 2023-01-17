621eb4f791d1b6b3f780425fb18098cbd3156eae
shindhha <99216728+shindhha@users.noreply.github.com>
2023-01-17 07:48:46 +0100

    Ajout idUser dans la table Medecins pour liers les connexion et les medecins

--- interne.sql
+++ interne.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS ListeVisites;
 DROP TABLE IF EXISTS Patients;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS Medecins;
+DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS Ordonnances;
 DROP TABLE IF EXISTS Visites;
 DROP TABLE IF EXISTS Cabinet;
@@ -9,15 +9,15 @@ DROP TABLE IF EXISTS Cabinet;
 
 
 CREATE TABLE Patients (
-	idPatient INT(6) AUTO_INCREMENT PRIMARY KEY,
+	idPatient INT AUTO_INCREMENT PRIMARY KEY,
 	numSecu CHAR(13) UNIQUE,
 	LieuNaissance VARCHAR(200),
 	nom VARCHAR(25),
 	prenom VARCHAR(25),
 	dateNaissance DATE,
 	adresse VARCHAR(50),
-	codePostal INT(5),
-	medecinRef INT(11),
+	codePostal INT,
+	medecinRef CHAR(11),
 	numTel INT(9),
 	email VARCHAR(50),
 	notes TEXT,
@@ -25,23 +25,24 @@ CREATE TABLE Patients (
 );
 
 CREATE TABLE Medecins (
-	idMedecin INT(6) AUTO_INCREMENT ,
+    idUser INT,
+	idMedecin INT AUTO_INCREMENT ,
 	numRPPS CHAR(11) UNIQUE,
 	nom VARCHAR(25),
 	prenom VARCHAR(25),
 	adresse VARCHAR(25),
-	numTel INT(9),
+	numTel INT,
 	email VARCHAR(100),
 	dateInscription DATE,
 	dateDebutActivites DATE,
 	activite VARCHAR(100),
-	codePostal INT(5),
+	codePostal INT,
 	ville VARCHAR(100),
 	lieuAct VARCHAR(100),
 	PRIMARY KEY (idMedecin)
 );
 CREATE TABLE Visites (
-	idVisite INT(6) AUTO_INCREMENT,
+	idVisite INT AUTO_INCREMENT,
 	motifVisite TEXT,
 	dateVisite DATE,
 	Description TEXT,
@@ -50,29 +51,30 @@ CREATE TABLE Visites (
 );
 
 CREATE TABLE ListeVisites (
-	idMedecin INT(6),
-	idPatient INT(6),
-	idVisite INT(6),
+	idMedecin INT,
+	idPatient INT,
+	idVisite INT,
 	PRIMARY KEY (idVisite,idPatient)
 );
 
 CREATE TABLE Cabinet (
 	adresse VARCHAR(25),
-	codePostal INT(5),
+	codePostal INT,
 	dateOuverture DATE
 );
 CREATE TABLE Ordonnances (
-	idVisite INT(6),
-	codeCIS INT(8),
+	idVisite INT,
+	codeCIS INT,
 	instruction TEXT,
 	PRIMARY KEY (idVisite,codeCIS)
 );
 CREATE TABLE users (
-	id INT(6) AUTO_INCREMENT PRIMARY KEY,
-	login VARCHAR(11),
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	login VARCHAR(11) UNIQUE,
 	`password` CHAR(32)
 
 );
+ALTER TABLE Medecins ADD CONSTRAINT FK_Medecins_Users FOREIGN KEY (idUser) REFERENCES users(id);
 ALTER TABLE Ordonnances ADD CONSTRAINT FK_Ordonnances_Medicaments FOREIGN KEY (codeCIS) REFERENCES CIS_BDPM(codeCIS);
 ALTER TABLE Patients ADD CONSTRAINT CK_Email_Patients CHECK (email LIKE '%@%.%');
 ALTER TABLE ListeVisites ADD CONSTRAINT FK_ListeVisites_Medecins FOREIGN KEY (idMedecin) REFERENCES Medecins(idMedecin);
